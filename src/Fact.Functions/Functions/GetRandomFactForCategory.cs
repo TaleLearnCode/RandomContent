f400200af6d44404d9db40a7c3c6773a28c9755c
Chad Green <chadgreen@chadgreen.com>
2024-06-09 09:29:44 -0400

    Retructure; Fact API not working, Quote API working

--- src/Fact.Functions/Functions/GetRandomFactForCategory.cs
+++ src/Fact.Functions/Functions/GetRandomFactForCategory.cs
@@ -13,6 +13,6 @@ public class GetRandomFactForCategory
 	public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "/random-facts/categories/{category}")] HttpRequest request, string category)
 	{
 		_logger.LogInformation("C# HTTP function triggered: {functionName}.", nameof(GetRandomFactForCategory));
-		return new OkObjectResult(FactServices.GetRandomFactForCategoryAsync(category);
+		return new OkObjectResult(FactServices.GetRandomFactForCategoryAsync(category));
 	}
 }
